411e0b1093015da11155ec773956895eae9fa8b0
Rafael23082 <raf.anderson777@gmail.com>
2025-07-26 22:02:45 +0700

    Landing page responsive update

--- src/components/DecorativePill.jsx
+++ src/components/DecorativePill.jsx
@@ -6,7 +6,7 @@ function DecorativePill({text, white, italic, absolute, centered, margin}){
         color = "white"
     }
     return(
-        <div className={`border border-${color} w-fit px-[1em] rounded-[20px] flex items-center ${margin ? "mt-[4em]": "mt-0"} ${absolute ? "absolute top-[20%] left-1/2 -translate-x-1/2": ""} whitespace-nowrap ${centered ? "mx-auto": ""}`}>
+        <div className={`border border-${color} w-fit px-[1em] rounded-[20px] flex items-center ${margin ? "mt-[4em]": "mt-0"} ${absolute ? "absolute top-[100px] left-1/2 -translate-x-1/2": ""} whitespace-nowrap ${centered ? "mx-auto": ""}`}>
             <div className="w-2 h-2 rounded-full bg-[#84fc1c]"></div>
             <p className={`text-${color} ml-[0.5em] text-[0.9rem] lg:text-[1.1rem] ${italic ? "italic": ""}`}>{text}</p>
         </div>
